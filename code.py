aa349071e7dcfb748ab5779d408a7b73ecfe6908
Dharma1133 <dharmateja9347@gmail.com>
2023-07-14 11:29:43 +0530

    changes to spelling misktakes

--- code.py
+++ code.py
@@ -1,2 +1,14 @@
-this is a python code.
-it can be used in desktop applicatios,gamming,ghraphs.
+This is Python code.
+It can be used in desktop applications, gaming, and graphics.
+In Python, there are 5 data types. They are
+1. Int
+2. String
+3. List
+4. Tuples
+5. Dictnories
+
+
+
+
+
+
